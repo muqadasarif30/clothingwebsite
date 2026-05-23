1e8854a2932345ef8525aed22b67609d2c5159e0
Muqadas <muqadasarif30@gmail.com>
2026-05-23 09:47:12 -0700

    First commit

---
+++ style.js
@@ -0,0 +1,10 @@
+function showPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+setTimeout(showPopup, 5000);
+
+
